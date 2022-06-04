616c18337fa7b5903224186365dbfaac8ff6cc89
hye-on <ain0103@naver.com>
2022-06-04 22:54:08 +0900

    1780

---
+++ Algorithm/Algorithm/1780종이의개수.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+
+int nn,temp;
+int num[2200][2200];
+
+int ans[3];
+int check;
+void division(int x, int y, int n)
+{
+
+	check = num[x][y];
+	for (int i = x; i < x + n; i++)
+		for (int j = y; j < y + n; j++)
+		{
+			if (num[i][j] != num[x][y])
+			{
+				
+				division(x, y, n/3);
+				division(x + n*1/3, y, n/3);
+				division(x + n*2/3, y, n/3);
+				division(x, y + n * 1 / 3, n / 3);
+				division(x + n * 1 / 3, y + n / 3, n / 3);
+				division(x + n * 2 / 3,y+ n / 3, n / 3);
+				division(x, y + n * 2 / 3, n / 3);
+				division(x + n * 1 / 3, y + n * 2 / 3, n / 3);
+				division(x + n * 2 / 3, y + n * 2 / 3, n / 3);
+				return;
+
+			}
+		}
+	ans[num[x][y] + 1]++;
+	return;
+}
+
+int main()
+{
+	cin >> nn;
+	
+	for (int i = 0; i < nn; i++)
+		for (int j = 0; j < nn; j++)
+			cin >> num[i][j];
+
+	division(0,0,nn);
+	
+	cout << ans[0] << "\n" << ans[1] << "\n" << ans[2];
+}
