8ffc5a2af86ee11ced7d715d094208ce408b6baf
hye-on <ain0103@naver.com>
2022-09-11 12:05:24 +0900

    11052

---
+++ Algorithm/Algorithm/11052카드구매하기.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int N;
+int num[10001];
+int dp[10001];
+int main()
+{
+	cin >>  N;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> num[i];
+		dp[i] = num[i];
+	}
+
+	
+	for (int i = 2; i <= N; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			dp[i] = max(dp[i], dp[j] + dp[i - j]);
+		}
+	}
+	cout<<dp[N];
+
+
+}
