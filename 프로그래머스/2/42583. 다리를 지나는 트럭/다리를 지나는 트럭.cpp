ffa570ea595597650a5f3a97f2653fb0719d010d
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-01-08 14:28:41 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,44 +1,32 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include  <queue> 
 using namespace std;
 
-queue<int>bridge;
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
     int truck_number = truck_weights.size();
     int total_weight =0;
     int truck_cnt=0;
-    int index=0;
-    for(int i = 0 ;i<1000000;i++){
-        
-        if(bridge.size() == bridge_length){
-            if(bridge.front()!=0)
-            {    
-                total_weight-=bridge.front();
-                truck_cnt--;
-            }
-            bridge.pop();
-                    
+    
+    for(int i=0 ; i<truck_number;i++){
+        if(total_weight + truck_weights[i] > weight  ){    
+            total_weight = truck_weights[i];
+            answer+= bridge_length;
         }
-        
-        if(total_weight + truck_weights[index] > weight || truck_cnt + 1 > bridge_length ){
-            bridge.push(0);
-            //cout<<total_weight<<endl;
-            continue;
-        }
-        
-        bridge.push(truck_weights[index]);
-        truck_cnt++;
-        total_weight+=truck_weights[index++];
-        if(index  == truck_number){
-            answer= i + bridge_length +1;
+        else if ( truck_cnt > bridge_length)
+            answer++;
+        else{
+            if(i==0)
+                answer+=bridge_length;
+            else
+                answer++;
+            total_weight += truck_weights[i];
             
-            break;
         }
+      truck_cnt++;
+      
     }
-
-
+    answer++;
     return answer;
 }
