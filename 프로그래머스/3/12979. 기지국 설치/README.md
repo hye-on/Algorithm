b57623b0571437fc2419c860fdb3134d932f8f7a
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-03-20 10:17:15 +0900

    [level 3] Title: 기지국 설치, Time: 0.08 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/README.md
+++ 프로그래머스/3/12979. 기지국 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기지국 설치 - 12979 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979) 
 
 ### 성능 요약
 
-메모리: 3.91 MB, 시간: 0.09 ms
+메모리: 4.02 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 20일 09:44:45
+2024년 03월 20일 10:17:14
 
 ### 문제 설명
 
--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -5,42 +5,29 @@ using namespace std;
 int solution(int n, vector<int> stations, int w)
 {
     int answer = 0;
-    bool chk =false;
-    if(stations[stations.size()-1]!=n){
-        stations.push_back(n); 
-        chk=true;
-    }
+
+    int prev = 1;
     int size = stations.size();
-    int range = 2*w+1;
-    if(stations[0]-1-w>0){
-        if((stations[0]-1-w)%range)
-            answer += (stations[0]-1-w)/range+1;
-        else
-            answer += (stations[0]-1-w)/range;
-    }
     int distance=0;
-    for(int i=1;i<size;i++){
-        
-        if(i==size-1){
-            distance = stations[i]-stations[i-1]-w;
-            if(!chk)
-                distance-=(w+1);
-        }
-        else
-            distance = stations[i]-stations[i-1]-range;
-        
-        
-        if(distance<0)
+    int range = 2*w+1;
+    for(int i=0;i<size;i++){
+        distance = stations[i]-prev-w;
+        // cout<<stations[i]<<" "<<prev<<endl;
+        // cout<<distance<<endl;
+        prev = stations[i]+w+1; 
+        if(distance<=0)
             continue;
-        if(distance%range){
-            answer+=distance/range+1;
-           // cout<<distance/range+1<<endl;
-        }
-        else{
-            answer+=distance/range;
-           // cout<<distance/range<<endl;
-        }
+        answer+=(distance/range);
+        if(distance%range!=0)
+            answer++;
         
+    }
+    
+    distance = n-stations[size-1]-w;
+    if(distance>0){
+        answer+=(distance/range);
+        if(distance%range)
+            answer++;
     }
 
     return answer;
