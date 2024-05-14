404e400685aefa0ac844015b75d4b3162a6a7156
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-05-14 22:03:17 +0900

    [Gold III] Title: 파티, Time: 4 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -4,30 +4,31 @@ using namespace std;
 
 
 int N, M, X;
-int d[1001][1001];
-vector<pair<int,int>>graph[1001];
+int d[2][1001];
+vector<pair<int,int>>graph[2][1001];
+
 
 int from, to, t;
 
-void dijkstra(int start) {
+void dijkstra(int start,int k) {
 	priority_queue<pair<int,int>>pq; //-비용, 현재노드
 
 	pq.push({ 0,start });
-	d[start][start] = 0;
+	d[k][start] = 0;
 	while (!pq.empty()) {
 
 		int dist = -pq.top().first;
 		int node = pq.top().second;
 
 		pq.pop();
-		if (d[start][node] < dist) //이미 구한 노드라면
+		if (d[k][node] < dist) //이미 구한 노드라면
 			continue;
-		for (int i = 0; i < graph[node].size(); i++) {
-			int cost = dist + graph[node][i].second;
+		for (int i = 0; i < graph[k][node].size(); i++) {
+			int cost = dist + graph[k][node][i].second;
 
-			if (cost < d[start][graph[node][i].first]) {
-				d[start][graph[node][i].first] = cost;
-				pq.push({ -cost,graph[node][i].first });
+			if (cost < d[k][graph[k][node][i].first]) {
+				d[k][graph[k][node][i].first] = cost;
+				pq.push({ -cost,graph[k][node][i].first });
 			}
 		}
 	}
@@ -39,28 +40,30 @@ int main() {
 	
 	for (int i = 0; i < M; i++) {
 		cin >> from >> to >> t;
-		graph[from].push_back({ to,t });
+		graph[0][from].push_back({ to,t });
+		graph[1][to].push_back({ from,t });
 
 	}
 
 	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <=N; j++) {
-			
-			d[i][j] = 1000000000;
-		}
+		
+		d[0][i] = 1000000000;
+		d[1][i] = 1000000000;
+		
 		
 	}
 	
-	for (int i = 1; i <= N; i++) {
-		dijkstra(i);
-	}
+	
+	dijkstra(X, 0);
+	dijkstra(X,1);
+	
 
 	
 	int answer = 0;
 	for (int i = 1; i <= N; i++) {
 		
-		if (answer < d[X][i] + d[i][X])
-			answer = d[X][i] + d[i][X];
+		if (answer < d[0][i] + d[1][i])
+			answer = d[0][i] + d[1][i];
 	}
 	cout << answer;
 
