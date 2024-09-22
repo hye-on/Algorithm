24d67cec9eb2a72663ae28be8d524903e5c4fa6e
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-09-23 00:53:41 +0900

    [Silver III] Title: 주유소, Time: 48 ms, Memory: 2804 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int m;
+long long ans;
+int n;
+vector<int> dis;
+vector<int>node;
+int main() {
+	cin >> n;
+	dis.resize(n - 1);
+	node.resize(n);
+	for (int i = 0; i < n-1; i++) {
+		cin >> dis[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> node[i];
+	}
+	m = node[0];
+	for (int i = 1; i < n; i++) {
+		ans += dis[i - 1] * m;
+		m = min(node[i], m);
+	}
+	cout << ans;
+}
