5704c6642fb61a19b85702c55229e6d6a3da204e
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-10-08 21:52:06 +0900

    [level 3] Title: 광고 삽입, Time: 242.53 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.cpp
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.cpp
@@ -2,13 +2,17 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
+vector<pair<long long,long long>> t; // i~i+1구간까지 중접된 개수, 시각
+priority_queue<pair<long long,long long>, vector<pair<long long,long long>>, greater<pair<long long,long long>>> pq;
+
 long long timeToInt(string time) {
-    int hs = stoi(time.substr(0, 2)) * 60 * 60;
-    int ms = stoi(time.substr(3, 2)) * 60;
-    int s = stoi(time.substr(6, 2));
+    int hs = stoi(time.substr(0,2)) * 60 * 60;
+    int ms = stoi(time.substr(3,2)) * 60;
+    int s  = stoi(time.substr(6,2));
     return hs + ms + s;
 }
 
@@ -19,53 +23,76 @@ string intToTime(long long num) {
     int m = num / 60;
     int s = num % 60;
     
-    if (h < 10) st += "0";
-    st += to_string(h) + ":";
-    if (m < 10) st += "0";
-    st += to_string(m) + ":";
-    if (s < 10) st += "0";
+    if (h <= 9)
+        st += '0';
+    st += to_string(h);
+    st += ":";
+    if (m <= 9)
+        st += '0';
+    st += to_string(m);
+    st += ":";
+    if (s <= 9)
+        st += '0';
     st += to_string(s);
     
     return st;
 }
 
+void insertAfterInt(string s) {
+    string startTime = s.substr(0,8);
+    string endTime = s.substr(9,8);
+    pq.push({timeToInt(startTime), 0}); //0이면 시작 시각
+    pq.push({timeToInt(endTime), 1});  //1이면 끝 시각
+}
+
 string solution(string play_time, string adv_time, vector<string> logs) {
-    long long play_duration = timeToInt(play_time);
-    long long adv_duration = timeToInt(adv_time);
-    
-    // 시청자 타임라인 배열 생성 (초 단위)
-    vector<long long> timeline(play_duration + 1, 0);
-    
-    // 로그에서 시작과 종료 시간에 맞게 배열 업데이트
-    for (auto log : logs) {
-        long long start_time = timeToInt(log.substr(0, 8));
-        long long end_time = timeToInt(log.substr(9, 8));
-        timeline[start_time]++;
-        timeline[end_time]--;
+    long long playTimeInt = timeToInt(play_time);
+    long long duration = timeToInt(adv_time);
+
+    if (logs.empty() || duration >= playTimeInt) {
+        return "00:00:00";
+    }
+
+    for (auto lo : logs) {
+        insertAfterInt(lo);
     }
     
-    // 시청 인원 누적
-    for (int i = 1; i <= play_duration; i++) {
-        timeline[i] += timeline[i - 1];
-    }
+    vector<long long> totalTime(playTimeInt + 1, 0);
     
-    // 시청 시간 누적
-    for (int i = 1; i <= play_duration; i++) {
-        timeline[i] += timeline[i - 1];
-    }
-    
-    // 광고 구간에서 최대 누적 시청 시간 찾기
-    long long max_view = timeline[adv_duration - 1];
-    long long max_time = 0;
-    
-    for (int i = adv_duration; i <= play_duration; i++) {
-        long long current_view = timeline[i] - timeline[i - adv_duration];
-        if (current_view > max_view) {
-            max_view = current_view;
-            max_time = i - adv_duration + 1;
+    while (!pq.empty()) {
+        long long time = pq.top().first;
+        int type = pq.top().second;
+        pq.pop();
+     
+        if (type == 0) { // 시작 시간
+            totalTime[time]++;
+        } else { // 종료 시간
+            totalTime[time]--;
         }
     }
     
-    // 가장 시청 시간이 많은 구간의 시작 시간을 반환
-    return intToTime(max_time);
+    // 누적 합 계산
+    for (int i = 1; i <= playTimeInt; i++) {
+        totalTime[i] += totalTime[i-1];
+    }
+    
+    // 구간 합 계산
+    // i까지의 합 
+    for (int i = 1; i <= playTimeInt; i++) {
+        totalTime[i] += totalTime[i-1];
+    }
+    
+    long long maxTime = totalTime[duration - 1];
+    long long ansInt = 0;
+    
+    for (long long i = duration; i < playTimeInt; i++) {
+        //i까지의 합 - j까지의 합 = j-i구간 합
+        long long currentTime = totalTime[i] - totalTime[i - duration];
+        if (currentTime > maxTime) {
+            maxTime = currentTime;
+            ansInt = i - duration + 1;
+        }
+    }
+
+    return intToTime(ansInt);
 }
