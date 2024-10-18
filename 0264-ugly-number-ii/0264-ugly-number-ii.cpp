0b9f20161ebb63d2cdc1e5011b1a590bdc4e6f6e
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-10-18 20:52:10 +0900

    Time: 3 ms (74.55%), Space: 7.9 MB (98.44%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,35 +1,36 @@
 int n;
-set<long long>s;
-long long t;
-long long ans;
+int ugly[1690]; 
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        for(int i=0;i<=30;i++){
-             long long pow2 = (1LL << i);  // 2의 i승
-                for(int j=0;j<=19;j++){
-                     long long pow3 = pow(3, j);  // 3의 j승
-                       if (pow2 > LLONG_MAX / pow3) break;  // long long 범위 초과 방지
-                    for(int k=0;k<=13;k++){
-                         long long pow5 = pow(5, k);  // 5의 k승
+     
+    int i2 = 0, i3 = 0, i5 = 0;
+  
+    int next2 = 2, next3 = 3, next5 = 5;
 
-                    if (pow2 * pow3 > LLONG_MAX / pow5) break;  // long long 범위 초과 방지
-
-                    t = pow2 * pow3 * pow5;  // 최종 못생긴 수 계산
-                    s.insert(t);  // set에 삽입하여 중복 제거
-                    }
-                }
-            }
-            int idx=0;
-        
-            for(auto it = s.begin();it!=s.end();it++){
-                idx++;
-                if(idx==n){
-                    ans = *it;
-                   break;
-                }
-            }
-            return ans;   
+    ugly[0] = 1; // 첫 번째 못생긴 수는 1
+  
+    for (int l = 1; l < n; l++) {
+        // 가능한 곱셈 결과 중에서 가장 작은 수를 선택
+        //작은 수부터 넣기 위해 
+        ugly[l] = min(next2, min(next3, next5));
+        // 인덱스에 따라서 곱셈 결과를 증가
+        if (ugly[l] == next2) {
+            i2 += 1;
+            next2 = ugly[i2] * 2;
+        }
+        if (ugly[l] == next3) {
+            i3 += 1;
+            next3 = ugly[i3] * 3;
+        }
+        if (ugly[l] == next5) {
+            i5 += 1;
+            next5 = ugly[i5] * 5;
+        }
     }
 
+    // n번째 못생긴 수를 출력
+         return ugly[n - 1];
+        }
+    
 };
