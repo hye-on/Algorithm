b27775c4d3f0887d8f79f0dc863e4495347f4453
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-10-22 15:03:40 +0900

    Time: 14 ms (26.66%), Space: 12.9 MB (40.04%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    //2:56
+    int minDistance(string word1, string word2) {
+        int fromSize = word1.size();
+        int toSize = word2.size();
+
+        vector<vector<int>>dp(fromSize+1,vector<int>(toSize+1,0));
+
+        for(int i=1;i<=fromSize;i++)
+            dp[i][0]= i;
+
+        for(int i=1;i<=toSize;i++)
+            dp[0][i]= i;
+
+        for(int i=1;i<=fromSize;i++){
+            for(int j=1;j<=toSize;j++){
+                if(word1[i-1]==word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else // 삭제, 교체, 추가 
+                    dp[i][j] = 1+ min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]}); 
+            }
+        }
+        return dp[fromSize][toSize];
+    }
+};
