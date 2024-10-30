bb76884181c0a22267acd14e842ff1cf1a8d20a2
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-10-30 19:35:42 +0900

    [Gold III] Title: 두 배열의 합, Time: 268 ms, Memory: 23960 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -9,7 +9,8 @@ int T;
 int n, m;
 //07:07 ~ 07:27
 
-//1 누적합
+//1 누적합 444ms
+//2 이분탐색
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -35,6 +36,8 @@ int main() {
 	cin >> m;
 	vector<int>b(m);
 	vector<int>bSum(m);
+	vector<int>bV;
+
 	map<int, int>bM;
 
 	t = 0;
@@ -42,11 +45,14 @@ int main() {
 		cin >> b[i];
 		t += b[i];
 		bSum[i] = t;
-		bM[bSum[i]]++;
+		bV.push_back(bSum[i]);
 		for (int j = 0; j < i; j++) {
-			bM[bSum[i] - bSum[j]]++;
+			bV.push_back(bSum[i] - bSum[j]);
 		}
 	}
+
+	//이분 탐색을 위해 정렬
+	sort(bV.begin(), bV.end());
 
 	//a 순회하면서 T-[a[i]] 존재하는지 찾기
 	long long ans=0;
@@ -55,11 +61,9 @@ int main() {
 		long long cntA = it->second;
 		int fV = T - vA;
 		
-		//찾는 값이 있으면
-		if (bM.find(fV)!= bM.end()) {
-			long long cntB = bM.find(fV)->second;
-			ans += cntA * cntB; //int * int 결과가 long 이라서 틀렸음. 결과가 long일때 주의하기
-		}
+		auto it1 = upper_bound(bV.begin(), bV.end(), fV);
+		auto it2 = lower_bound(bV.begin(), bV.end(), fV);
+		ans += cntA * (it1 - it2);
 	}
 
 	cout << ans;
