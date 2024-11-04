b594883af4c8265c381d1b400111124aac19e2e3
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-11-04 16:53:22 +0900

    [Platinum V] Title: 히스토그램, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.cc
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -5,42 +5,51 @@
 
 using namespace std;
 
-// 양방향으로 검사
-// 스택은 오름차순으로 쌓기
-
-// 들어오는 애보다 스택의 top이 더 크면 pop
-// 같으면 arr 값 += 
+//분할 정복
+//왼쪽, 오른쪽, 걸치기
+// O(N log N)
 
 int n;
-stack<pair<int, int>>s;
+vector<int>histogram(100000);
+
+int recursion(int start, int end) {
+	if (start == end)
+		return histogram[start];
+
+	int mid = (start + end) / 2;
+	int left = mid, right = mid + 1;
+	int high = min(histogram[mid], histogram[mid + 1]);
+	int maxN = 2 * high;
+	while (start<left || right < end) {
+
+	
+		// 오른쪽이 더 크거나 왼쪽이 더 갈 곳이 없을 때
+		if (right < end && (start == left || histogram[left - 1] < histogram[right + 1])) {
+			right++;
+			high = min(high, histogram[right]);
+		}
+		else {
+			left--;
+			high = min(high, histogram[left]);
+		}
+		maxN = max(maxN, high * (right - left + 1));
+	}
+
+	return max({ maxN, recursion(start,mid),recursion(mid + 1,end) });
+}
 int main() {
+
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
+
 	cin >> n;
 
-	vector<int>histogram(n+1);
-	
 	for (int i = 0; i < n; i++) {
 		cin >> histogram[i];
-	
 	}
 
-	histogram[n] = - 1;
-		
-	stack<pair<int, int>>st; // 높이,idx
-	int ans = 0;
-	for (int i = 0; i <= n; i++) {
-		int minPos = i;
-		while (!st.empty() && st.top().first >= histogram[i]) {
-			auto [h, pos] = st.top();
-			ans = max(h * (i - pos), ans);
-			minPos = min(i, st.top().second); // 2, 1일때 2를 pop함 하지만 1의 높이로 0번째 인덱스까지 그릴 수 있음
-			st.pop();
-		}
+	cout << recursion(0, n - 1);
 
-		st.push({ histogram[i],minPos });
-	}
-	cout << ans;
 }
