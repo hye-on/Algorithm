540b6a82a185e9bfe02bc7afb7834da970078b7c
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-11-27 15:36:12 +0900

    [level 3] Title: 산 모양 타일링, Time: 1.39 ms, Memory: 7 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
@@ -11,14 +11,16 @@ int solution(int n, vector<int> tops) {
     
     dp[1]=1;
     dp[2]=2;
-    if(tops[0])
-        dp[2]++;
+    if(tops[0])        dp[2]++;
     for(int i=3;i<=total;i++){
+
         dp[i] = (dp[i-1]+dp[i-2])%10007;
+        //위에 삼각형이 있을 수 있음 
         if(i%2==0){
             idx  = i/2-1;
+            //있다면 
             if(tops[idx]){
-                dp[i]=(dp[i-1]*2+dp[i-2])%10007;
+                dp[i]+=dp[i-1]%10007;
             }
         }
        
