e8beca297493ecf981979f2ab70ac279edc5a043
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-11-28 16:40:11 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -2,64 +2,56 @@
 using namespace std;
 
 string solution(int n, int m, int x, int y, int r, int c, int k) {
-    // 각 방향으로 가야하는 최소 횟수 계산
-    int down = max(0, r-x);
-    int left = max(0, y-c);
-    int up = max(0, x-r);
-    int right = max(0, c-y);
-    
-    // 불가능한 경우 체크
-    int minDist = down + left + up + right;
-    if(k < minDist || (k - minDist) % 2 == 1) 
-        return "impossible";
-        
-    // 상쇄 이동 횟수
-    int pair = (k - minDist) / 2;
-    int curX = x, curY = y;
-    string answer = "";
-    
-    for(int i = 0; i < k; i++) {
-        // down (아래로 갈 수 있고, 필수 이동이 남았거나 상쇄 이동이 가능할 때)
-        if(curX < n && (down > 0 || pair > 0)) {
-            answer += 'd';
-            curX++;
-            if(down > 0) down--;
-            else {
-                pair--;
-                up++;
-            }
-        }
-        // left
-        else if(curY > 1 && (left > 0 || pair > 0)) {
-            answer += 'l';
-            curY--;
-            if(left > 0) left--;
-            else {
-                pair--;
-                right++;
-            }
-        }
-        // right
-        else if(curY < m && (right > 0 || pair > 0)) {
-            answer += 'r';
-            curY++;
-            if(right > 0) right--;
-            else {
-                pair--;
-                left++;
-            }
-        }
-        // up
-        else if(curX > 1 && (up > 0 || pair > 0)) {
-            answer += 'u';
-            curX--;
-            if(up > 0) up--;
-            else {
-                pair--;
-                down++;
-            }
-        }
-    }
-    
-    return answer;
+   // 각 방향으로의 최소 이동 횟수 계산
+   int down = max(0, r-x);
+   int left = max(0, y-c);
+   int up = max(0, x-r);
+   int right = max(0, c-y);
+   
+   // 최소 이동 거리
+   int dist = down + left + up + right;
+   if(k < dist || (k - dist) % 2 == 1) return "impossible";
+   
+   int spare = (k - dist) / 2;  // 여분의 이동 횟수
+   string answer = "";
+   
+   // 1. down 먼저
+   for(int i = 0; i < down; i++) 
+       answer += 'd';
+   
+   // 아래로 더 갈 수 있으면 최대한 이동
+   int extra_down = 0;
+   while(spare > 0 && x + down + extra_down + 1 <= n) {
+       answer += 'd';
+       extra_down++;
+       spare--;
+   }
+   
+   // 2. left 처리
+   for(int i = 0; i < left; i++) 
+       answer += 'l';
+       
+   // 왼쪽으로 더 갈 수 있으면 최대한 이동
+   int extra_left = 0;
+   while(spare > 0 && y - left - extra_left - 1 >= 1) {
+       answer += 'l';
+       extra_left++;
+       spare--;
+   }
+   
+   // 3. spare가 남았다면 rl 반복으로 소진
+   while(spare > 0) {
+       answer += "rl";
+       spare--;
+   }
+   
+   // 4. right 처리 (extra_left만큼 더 이동)
+   for(int i = 0; i < right + extra_left; i++) 
+       answer += 'r';
+   
+   // 5. up 처리 (extra_down만큼 더 이동)
+   for(int i = 0; i < up + extra_down; i++) 
+       answer += 'u';
+   
+   return answer;
 }
