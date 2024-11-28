30c1d755066e761358492875ca00160fd89e2f21
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-11-28 14:37:04 +0900

    [Gold II] Title: 컵라면, Time: 76 ms, Memory: 6244 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.cc
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -4,11 +4,15 @@
 
 using namespace std;
 
-//1:20 ~2:17
+//1:20 ~2:36
 int n;
 
 priority_queue<pair<int, int>>pq;//데드라인, 컵라면
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	cin >> n;
 	int a = 0, b = 0;
 	int ans = 0;
@@ -21,7 +25,7 @@ int main() {
 	}
 
 	priority_queue<int>frontDay;
-	while (today>=1 ) {
+	while (today>=1 ) { //틀린 이유 : pq가 empty더라도 계속 돌아야함 
 		
 		
 		while (!pq.empty() &&today <= pq.top().first) {
