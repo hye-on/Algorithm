4cf6bedc248ca59fb948333db50391388f5b8100
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-11-29 15:51:25 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 5.09 ms, Memory: 5.66 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.cpp
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.cpp
@@ -1,74 +1,51 @@
+
 #include <string>
 #include <vector>
-#include <iostream>
+#include <cstring>
 using namespace std;
 
-long long total;
-long long depth=0;
-string testS;
-long long ans;
-//2~2:45 - 이어서 풀기
-string numberTo01(long long num){
-    string tmp="";
-    while(num>0){
-        if(num%2==1)
-            tmp ='1'+tmp;
-        else
-            tmp= '0' + tmp;
-        num/=2;
-    }
-    return tmp;
-}
-long long findDepth(int n){
-    int n2=1;
-    int d=0;
-    int tt=0;
-    while(n>0){
-        n-=n2;
-        tt+=n2;
-        n2*=2;   
-        d++;
-    }
-    total= tt;
-    return d;
-}
-bool isAllDummy(int start, int end) {
-    for (int i = start; i <end; i++) {
-        if (testS[i] == '1') {
-            return false;  
-        }
-    }
-    return true;  
-}
-//idx위치는 1이어야 한다. depth가 1일 때 제외
-void tree(long long start,long long end, long long depth){
-    int idx = (start+end)/2;
-   
-    if(testS[idx]=='0' && depth!=1){
-        if (!isAllDummy(start, end)) {
-          
-            ans = 0;  // 더미 노드 아래에 1인 노드가 있으면 오답 처리
-        }
-    }
-    if(depth==1)
-        return;
+long long y = 0;
+bool check[64], visit[64];
+//조건 : 0아래에는 1이 있으면 안된다.
+// 0이면 dfs를 중단하므로 그 아래 1이 있더라도 y를 만드는데 반영되지 않음
+// 즉 x랑 y랑 다른 숫자가 됨. 
+//dx : now를 기준으로 양 옆에 자식 노드까지의 거리 
+void dfs(int now, int dx){
+    visit[now] = true; y += (1LL << now);
     
-    tree(start,idx,depth-1);    
-    tree(idx+1,end,depth-1);    
+    //next는 자식 노드 
+    for (int next : {now - dx, now + dx})
+        
+    //1이고 방문한 적 없으면 
+    if (check[next] && !visit[next])
+        dfs(next, dx / 2);
 }
 vector<int> solution(vector<long long> numbers) {
     vector<int> answer;
-    
-    for(int i=0;i<numbers.size();i++){
-        testS = numberTo01(numbers[i]);
-        depth = findDepth(testS.size());
-        while(testS.size()<total)
-            testS = '0' + testS;
-        ans=1;
-        tree(0,total,depth);
-        answer.push_back(ans);
+    for (long long x : numbers){
+        bool flag = false;
+        memset(check, 0, sizeof(check));
+        //비트마스크를 통해 2진수로 바꾼다.
+        //long long은 64비트 0~63비트 사용함
+        // 63번째 비트는 부호를 결정하는 비트라서 제외하고 62번째 비트까지 사용
+        for (int k = 0; k < 63; k++){
+            if ((1LL << k) & x)
+                check[k] = true;
+        }
+        //루트가 될 수 있는 노드 만약 depth가 4면 7이 루트 노드 
+        //만약 포화이진트리 노드 개수가 64개면 중간에 있는 31이 루트 노드 (0부터 시작)
+        // n번째가 루트노드일 때 dfs로 숫자를 만들기
+        for (int root : {0, 1, 3, 7, 15, 31})
+        if (check[root]){
+            y = 0;
+            memset(visit, 0, sizeof(visit));
+            dfs(root, (root + 1) / 2);
+            // 숫자가 같으면 flag를 true로 만들어서 탈출
+            //다르면 다음 root로 숫자를 만들어서 검사 
+            if (flag = (x == y)) break;
+        }
 
-        
+        answer.push_back(flag);
     }
     return answer;
 }
