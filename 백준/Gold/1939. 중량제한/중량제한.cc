cfbe2936098f462d53c55dbe6f924771c9aa8e55
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-12-24 18:08:23 +0900

    [Gold III] Title: 중량제한, Time: 36 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.cc
+++ 백준/Gold/1939. 중량제한/중량제한.cc
@@ -1,57 +1,59 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <algorithm>
+#include<queue>
+#include<algorithm>
 using namespace std;
 
+//5:00 ~ 
+
+//다익스트라
+
+int a,b,c;
 int n,m;
 int from,to;
 int dist[100001];
 
-void dijkstra(int start, vector<vector<pair<int,int>>>& link){
-    priority_queue<pair<int,int>> pq; //비용, 노드
-    
-    fill(dist, dist+n+1, 0);
-    
-    pq.push({2000000000,start});
-    dist[start] = 2000000000;
-    
+void dijkstra(int start, vector<vector<pair<int,int>>>&link){
+    priority_queue<pair<int,int>>pq;//비용, 노드
+
+    pq.push({1000000001,start});
+    dist[start]=1000000001;
     while(!pq.empty()){
-        int d = pq.top().first;
+        int d =  pq.top().first;
         int cur = pq.top().second;
         pq.pop();
         
-        if(dist[cur] > d)
+        if(dist[cur]>d)
             continue;
-            
-        for(auto& next : link[cur]){
-            int nextNode = next.first;
-            int nextCost = min(next.second, d);  
-            
-            if(dist[nextNode] < nextCost){
-                dist[nextNode] = nextCost;
-                pq.push({nextCost, nextNode});
+       
+        for(int i=0;i<link[cur].size();i++){
+            int next = link[cur][i].first;
+            int nextCost = min(link[cur][i].second,d);
+          
+            if(dist[next] < nextCost ){
+               
+                dist[next] = nextCost;
+                pq.push({nextCost,next});
             }
         }
+        
     }
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    cin >> n >> m;
-    vector<vector<pair<int,int>>> link(n+1);
-    
-    for(int i=0; i<m; i++){
-        int a,b,c;
-        cin >> a >> b >> c;
+    cin>>n>>m;
+    vector<vector<pair<int,int>>>link(n+1);
+    for(int i=0;i<m;i++){
+        cin>>a>>b>>c;
+        
         link[a].push_back({b,c});
         link[b].push_back({a,c});
     }
-    cin >> from >> to;
+    cin>>from>>to;
    
     dijkstra(from,link);
-    cout << dist[to];
+    cout<<dist[to];
     return 0;
 }
