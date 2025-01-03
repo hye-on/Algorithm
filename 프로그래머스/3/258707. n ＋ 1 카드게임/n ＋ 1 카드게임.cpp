781a69fba1aa1f2efd5fe0852acc30140af5d9b5
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-01-03 21:55:43 +0900

    [level 3] Title: n + 1 카드게임, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.cpp
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.cpp
@@ -1,85 +1,70 @@
 #include <string>
 #include <vector>
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-//1:14  ~ 2:59 = 1시간 45분
-//처음 세팅
-
-
-// 1 2 3 4 5 6 7 8 9 10 11 12 13
-// 짝이 정해져 있다.
-
-bool num[1001];
-bool st[1001];
-
-int p1,p2;
-bool nocard=false;
+vector<bool>visit(1001);
+vector<bool>my(1001);
 int solution(int coin, vector<int> cards) {
-    int answer = 0;
+    int answer = 1;
     int n = cards.size();
-    // n/3은 초기에 가지고 있음 
-    int k=0,k2=0;
+   
+    int pair=0, st_pair=0;
+    // 1개를 뽑아서 pair를 만들 수 있다면 구매
+    // 2개를 뽑아서 pair를 만들 수 있다면 보관
+    // 아직 다른 짝이 나오지 않았다면 나왔다는 표시만 해준다.
+    
+    
+     // n/3장 가진다. 
     for(int i=0;i<n/3;i++){
-        if(num[n+1 - cards[i]])
-            k++;
-        num[cards[i]] = true;
+        if(visit[n+1-cards[i]])
+            pair++;
+        
+        visit[cards[i]]=true;
+        my[cards[i]]=true;
+      
     }
    
-    int r=1;
-
-    for(int i=n/3;i<cards.size();i+=2){
-        p1 = cards[i];
-        p2 = cards[i+1];
+    for(int i=n/3;i<n;i+=2){
+        int c1 = cards[i];
+        int c2 = cards[i+1];
         
-        //만약에 n + 1만들 수 있으면 사기
-        if(coin>0 && num[n+1 - p1])
-        {   
-            
+        if(coin>0 && my[n+1-c1]){
+           
             coin--;
-            num[p1]=false;
-            num[n+1-p1] = false;
-            k++;
-        }//앞에 나왔던 것까지 같이 산다면 패스할 수 있음 -> 후보로 등록
-        else if(coin> 1 &&st[n+1 - p1]){ 
-            k2++;
+            pair++;
+        }else if(visit[n+1-c1]){
+             st_pair++;
         }
-         
-        if(coin >0 && num[n+1 - p2])
-        {   
+        visit[c1]=true;
+        if(coin>0 && my[n+1-c2]){
+          
             coin--;
-            num[p2]=false;
-            num[n+1-p2] = false;
-            k++;
-        }
-        else if(coin> 1 &&st[n+1 - p2]){     
-            k2++;
-        }
+            pair++;
+        }else if(visit[n+1-c2]){
+           st_pair++;
+         }
+       
         
-        //앞에 나왔던 것들을 표시
-        st[p1]=true;
-        st[p2]=true;
+        // if(c1+c2==(n+1))
+        //     st_pair++;  
+        //  }
         
-        //뽑은 2개끼리 n+1이 될 수 있다면 후보로 표시
-        if(coin>1 &&(p1+p2)== (n+1) ){
-            k2++;
-        }
-            
-        if(k>0){
-            //가지고 있던 짝(n+1) 씀        
-            k--; 
-            r++;
-            
-        }else if(coin>1 && k2>0){ //없다면 가능한후보 씀. 그리고 coin-=2
-            k2--;
+        visit[c2]=true;
+        
+        if(pair>0){
+            answer++;
+            pair--;
+        }else if(coin>=2 && st_pair>0){
+            answer++;
             coin-=2;
-            r++;
-        }
-        else{
-            //라운드 진행못하면 끝
+            st_pair--;
+        }else
             break;
-        }
-
         
-    }
-    return answer = r;
+    }    
+   
+    
+    
+    return answer;
 }
