b8b5fb60690eb448b3f55f2a82ed5ef13a5d6aef
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-01-11 17:03:44 +0900

    [level 3] Title: 외벽 점검, Time: 25.74 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
@@ -1,159 +1,53 @@
-#include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
-//완전 탐색
-// dist에서 1개 고르고  
-// dist에서 2개 고르고 
-
-//dist에서 8개 고르고 
-
-//순차적으로 체크 1 5 6 10  1~5, 6~10
-int n,m;
-vector<int> weak;
-vector<int> dist;
-//int pick[8];
-bool visit[8];
-
-
-// void backtracking(int depth,int cnt,int idx){
-//     if(depth==cnt){
-//         return;
-//     }
-//     for(int i =0;i<dist.size();i++){
-//         if(visit[i])
-//             continue;
-//         visit[i] =true;
-//         pick[i] = dist[i];
-//         visit[i]=false;
-//     }
-        
-// }
-// vector<int> pickF(int cnt,vector<int>& dist){
+//더 좋은 풀이 
+int solution(int n, vector<int> weak, vector<int> dist) {
+    int answer = 1e8;
+    int w_size = weak.size();
     
-// }
-vector<int>pick2;
-vector<int>pick;
-int answer;
-
-bool findAns;
-
-void rotate(int srt,int k){
-    int idx =srt; //weak시작위치
-    int size = pick[0];
-    int start = weak[srt] ;
-    int end = weak[srt] + size;
-    int cnt =0;
-    int didx=0;
-    //m개가 되면 다 커버한것
+    //원형 탐색을 위한 전처리 
+    for(int i=0; i<w_size-1; ++i) {
+        weak.push_back(weak[i] + n);
+    }
     
-    while(true){
-       
-        if(idx<weak.size() && start<=weak[idx] && weak[idx]<=end){
-            cnt++;
-            idx++;
-          
-        }else{
+    sort(dist.begin(), dist.end()); 
+    
+    do {
+    //모든 취약점을 각각 시작점에서 해서 검사한다. 
+        for(int i=0; i<w_size; ++i) { 
+        //시작 취약점과 마지막 취약점 (1) [1,5,6,10]이면 1과 10 
+            int dist_range = weak[i];
+            int end = weak[i+w_size-1];
             
-            didx++;
-            start = weak[idx];
-            end = weak[idx] + pick[didx];
-        }
-       
-        if(didx==k || cnt==m){
-            break;
-        }
-    }
-    
-    if(cnt==m){
-        answer=k;
-    }
-    
-    if(answer>0)
-        findAns=true;
-        
-}
-
-void rotate_op(int srt,int k,vector<int>tmp){
-    int idx =srt; //weak시작위치
-    int size = pick[0];
-    int start = weak[srt] ;
-    int end = weak[srt] + size;
-    int cnt =0;
-    int didx=0;
-    //m개가 되면 다 커버한것
-    vector<int>weak = tmp;
-    reverse(weak.begin(),weak.end());
-    while(true){
-       
-        if(idx<weak.size() && start<=weak[idx] && weak[idx]<=end){
-            cnt++;
-            idx++;
-          
-        }else{
-            
-            didx++;
-            start = weak[idx];
-            end = weak[idx] + pick[didx];
-        }
-       
-        if(didx==k || cnt==m){
-            break;
-        }
-    }
-    
-    if(cnt==m){
-        answer=k;
-    }
-    
-    if(answer>0)
-        findAns=true;
-        
-}
-int solution(int _n, vector<int> _weak, vector<int> _dist) {
-
-    n =_n;
-    m = _weak.size();
-    weak = _weak;
-    dist = _dist;
-    
-    sort(dist.rbegin(),dist.rend()); //내림차순
-    for(int i=0;i<_weak.size();i++){
-        weak.push_back(_weak[i]+n);
-       
-    }
-    
-    
-    sort(weak.begin(),weak.end());
-        for(int i=0;i<dist.size();i++){
-            //dist에서 i개 고름
-            pick = pick2;
-            //2개 가능하다면 제일 큰거 2개 고르면 됌
-            for(int k=0;k<=i;k++){
-                pick.push_back(dist[k]);
-            } 
-            sort(pick.begin(),pick.end());
-
-        do{
-            for(int k=0;k<m;k++){
-
-                rotate(k,i+1);
-              //  rotate_op(k,i+1,weak);
-                if(findAns)
+            for(int j=0; j<dist.size(); ++j) {
+                dist_range += dist[j]; // j번째 친구가 커버하는 영역
+                
+                // 모든 지점 점검 마쳤을 경우
+                // 즉, (1) 에서 10번 취약점까지 다 커버한 경우
+                if(dist_range >= end) { 
+                // 2개에서 커버가 끝날 수 있음 
+                    answer = min(answer, j+1);
                     break;
+                }
+                //(3)
+                // 외벽 점검을 마치지 않았지만 더이상 이동할 수 없으면 다음 지점으로 이동
+                //dist j번째 친구로 커버할 수 없는 지점 찾아서 영역 바꿔주기
+                // ex) start = 5, weak [1,3,5,6,10] . 6부터 커버 할 수 없음
+                // -> start = weak[3]
+                for(int z=i+1; z<i+w_size; ++z) { //시작위치가 i
+                    if(weak[z] > dist_range) {
+                        dist_range = weak[z];
+                        break;
+                    }
+                }
             }
-        }while(next_permutation(pick.begin(),pick.end()));
-            if(findAns)
-                break;
-
-        
-            
-    }
+        }
+    } while(next_permutation(dist.begin(), dist.end()));
     
-    if(!findAns)
-        answer=-1;
+    if(answer == 1e8) return -1;
+    
     return answer;
 }
