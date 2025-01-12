cb003c2b17948390f7f7e9566cab718443411a51
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-01-12 17:10:21 +0900

    [Gold I] Title: 그래프 트리 분할, Time: 148 ms, Memory: 15124 KB -BaekjoonHub

--- 백준/Gold/22954. 그래프 트리 분할/그래프 트리 분할.cc
+++ 백준/Gold/22954. 그래프 트리 분할/그래프 트리 분할.cc
@@ -5,13 +5,13 @@
 using namespace std;
 
 //2:14
-
+// 순차적으로 간산을 삭제해서 연결된 각 노드 2개로 bfs
+//정점번호 +  간선 번호 출력
 int n,m;
-vector<int>tree[100001];
-vector<int> treeEdges[100001];
-vector<pair<int,int>>edges[100001]; // {노드 번호, 간선 번호}
-vector<pair<int,int>>link;
-int nodeNum[100001];
+vector<int>tree[100002];
+vector<int> treeEdges[100002];
+vector<pair<int,int>>edges[100002]; // {노드 번호, 간선 번호}
+int nodeNum[100002];
 
 int treeNum;
 int leafNode;
@@ -72,12 +72,10 @@ int main() {
      fill(nodeNum,nodeNum+100001,0);
         
      nodeNum[leafNode]=2;
-        
-     int startN=1;
-     for(int i=1;i<=n;i++){
-         if(i!=leafNode)
-             startN = i;
-     }
+    int startN =1;
+     if(edges[leafNode].size()>0)   
+         startN= edges[leafNode][0].first;
+     
     
      tree[2].push_back(leafNode);
      bfs(startN,1);
@@ -92,24 +90,24 @@ int main() {
     //트리 개수
     cout<<tree[1].size()<<' '<<tree[2].size()<<'\n';
     //노드 번호
-    sort(tree[1].begin(), tree[1].end());
+   // sort(tree[1].begin(), tree[1].end());
     
     for(auto t :tree[1])
         cout<<t<<' ';
     cout<<endl;
     //간선 번호
-    sort(treeEdges[1].begin(), treeEdges[1].end());
+   // sort(treeEdges[1].begin(), treeEdges[1].end());
     for(auto e:treeEdges[1])
         cout<<e<<' ';
     cout<<endl;
     //노드 번호
-    sort(tree[2].begin(), tree[2].end());
+  //  sort(tree[2].begin(), tree[2].end());
     
     for(auto t :tree[2])
         cout<<t<<' ';
     cout<<endl;
     //간선 번호
-     sort(treeEdges[2].begin(), treeEdges[2].end());   
+  //   sort(treeEdges[2].begin(), treeEdges[2].end());   
     for(auto e:treeEdges[2])
         cout<<e<<' ';
 
