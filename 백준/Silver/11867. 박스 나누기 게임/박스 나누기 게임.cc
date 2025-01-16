cac77c52ce9615b7526b6107fd1ba89a66998b43
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-01-16 14:53:48 +0900

    [Silver II] Title: 박스 나누기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11867. 박스 나누기 게임/박스 나누기 게임.cc
+++ 백준/Silver/11867. 박스 나누기 게임/박스 나누기 게임.cc
@@ -1,35 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+
 using namespace std;
 
-const int MAX_N=101;
-
-int N;
-int dp[MAX_N][MAX_N];
-
-int dfs(int A, int B) {
-    // win condition
-    if (A == 2 || B == 2)
-        return 1;
-
-    int &ret = dp[A][B];
-    if (ret != -1) return ret;
-
-    ret = 0;
-    // Choose first Box
-    for (int i=1; i<A; i++)
-        ret |= !(dfs(i, A-i));
-    // Choose Second Box
-    for (int i=1; i<B; i++)
-        ret |= !(dfs(i, B-i));
-
-    return ret;
-}
+int n,m;
 
 int main() {
-    memset(dp, -1, sizeof(dp));
-    int A, B;
-    scanf("%d%d", &A, &B);
-    int ret = dfs(A, B);
-    puts(ret == 1 ? "A":"B");
+    
+    cin>>n>>m;
+
+    if(n%2==0 || m%2==0)
+        cout<<'A';
+    else
+        cout<<'B';
+   
     return 0;
 }
