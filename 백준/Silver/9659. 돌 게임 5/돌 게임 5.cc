0be161766588043708a0b917b837206fed002f1b
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-01-16 15:01:28 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+long long n;
+
+int main() {
+    cin>>n;
+    if(n%2==1)
+        cout<<"SK";
+    else
+        cout<<"CY";
+   
+    return 0;
+}
