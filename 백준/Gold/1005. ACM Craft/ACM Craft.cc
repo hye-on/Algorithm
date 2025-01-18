da4e99477eb51dd318c959ec459d9b21a0397ee6
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-01-18 21:35:00 +0900

    [Gold III] Title: ACM Craft, Time: 484 ms, Memory: 2720 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cc
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cc
@@ -4,64 +4,73 @@
 #include <algorithm>
 using namespace std;
 
-
-//8:57
-//bfs
 int T;
-int N,K;
+int N, K;
 int cost[1001];
-vector<int>edges[1001];
+vector<int> edges[1001];  
 int answer;
-int dist[1001]; //max가 들어가야함
+int dist[1001];            
+int inDegree[1001];       
 
-void bfs(int start){
-    queue<pair<int,int>>q;
-    q.push({start,cost[start]});
-    dist[start] = cost[start];
-   
-    while(!q.empty()){
-        int cur = q.front().first;
-        int d = q.front().second;
+//위상정렬 풀이
+void bfs(int start) {
+    queue<int> q;
+
+    for (int i = 1; i <= N; i++) {
+        if (inDegree[i] == 0) {
+            q.push(i);
+            dist[i] = cost[i]; 
+        }
+    }
+
+    while (!q.empty()) {
+        int cur = q.front();
         q.pop();
-      
-        if(dist[cur]<d)
-            continue;
-        
-        answer = max(answer,d);
-        for(int i=0;i<edges[cur].size();i++){
-            int nxt = edges[cur][i];
-            int nxtCost = d + cost[nxt];
-          
-            if(dist[nxt]<nxtCost){
-                dist[nxt] = nxtCost;
-                q.push({nxt,nxtCost});
+
+        for (int nxt : edges[cur]) {
+            dist[nxt] = max(dist[nxt], dist[cur] + cost[nxt]);
+
+            inDegree[nxt]--;
+            if (inDegree[nxt] == 0) {
+                q.push(nxt);
             }
         }
     }
-}
-int main() {
-    cin>>T;
-    while(T--){
-        cin>>N>>K;//건물개수, 간선 개수
-        for(int i=1;i<=N;i++)
-            cin>>cost[i];
 
-        int a=0,b=0;
-        
-        for(int i=0;i<K;i++){
-            cin>>a>>b;
-            edges[b].push_back(a);
-        }
-        int start=0;
-        cin>>start;
-        answer =0;
-        fill(dist,dist+1001,0);
-        bfs(start);
+    answer = dist[start];
+}
+
+int main() {
+    cin >> T;
+
+    while (T--) {
+        cin >> N >> K; // 건물 개수, 간선 개수
+
+        // 초기화
         for (int i = 1; i <= N; i++) {
-            edges[i].clear();  
+            edges[i].clear();
+            dist[i] = 0;
+            inDegree[i] = 0;
         }
-        cout<<answer<<'\n';
-        
+
+        for (int i = 1; i <= N; i++) {
+            cin >> cost[i];
+        }
+
+        int a, b;
+        for (int i = 0; i < K; i++) {
+            cin >> a >> b;
+            edges[a].push_back(b);  
+            inDegree[b]++;
+        }
+
+        int start;
+        cin >> start;
+
+        bfs(start);
+
+        cout << answer << '\n';
     }
+
     return 0;
 }
