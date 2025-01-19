5c07328819586fba8010c919d3c55648a545752f
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-01-19 21:55:19 +0900

    [Gold II] Title: 험난한 등굣길, Time: 244 ms, Memory: 72384 KB -BaekjoonHub

--- 백준/Gold/26009. 험난한 등굣길/험난한 등굣길.cc
+++ 백준/Gold/26009. 험난한 등굣길/험난한 등굣길.cc
@@ -1,105 +1,106 @@
 #include <iostream>
 #include <algorithm>
-#include<cmath>
-#include<vector>
-#include<queue>
-#include<tuple>
-
+#include <queue>
 using namespace std;
 
-//06:00
+//120억 개를 마크해야해서 시간초과
+//테두리만 마크하자
+//9:00
+int map[3001][3001];
+int range[3001][3001];
+int n,m;
+int k;
+int answer=-1;
 
+void mark(int y,int x,int d){
+    int dy[] ={-1,1,1,-1};
+    int dx[] ={1,1,-1,-1};
+    vector<pair<int,int>>pos = {{y,x-d},{y-d,x},{y,x+d},{y+d,x}};
 
-//모든 정체구역을 표시해주면 시간초과
-//경계만 표시해주기. 중심부를 기준으로 다이아몬드 크기
-//최악 1500 * 루트2 * 4 * 3000  
-
-//최적화 : 거리순으로 내림차순으로 정렬해서 안그려도 되면 패스
-
-int n, m, k;
-vector<vector<int>>map;
-bool exAns;
-bool OOB(int y,int x) {
-	if (y >= n || y < 0 || x >= m || x < 0)
-		return true;
-	return false;
+ 
+    for(int i=0;i<4;i++){
+    
+        int curY = pos[i].first;
+        int curX = pos[i].second;
+     
+        for(int j=0;j<=d;j++){
+            if(!(curY>n || curY<=0 || curX>m || curX<=0))
+               map[curY][curX]=1;
+            curY+=dy[i];
+            curX+=dx[i];
+        }
+    }
+   
 }
 
-int dy[] = { 0,1,0,-1 };
-int dx[] = { 1,0,-1,0 };
+void bfs(){
 
-void bfs() {
-	
-	queue<tuple<int,int,int>>q;
-	q.push({ 0,0,0 });
-	map[0][0] = 1;
-	while (!q.empty()) {
-		int curY = get<0>(q.front());
-		int curX = get<1>(q.front());
-		int d = get<2>(q.front());
-		q.pop();
+    int dy[] ={0,1,0,-1};
+    int dx[] = {1,0,-1,0};
+    
+    queue<pair<pair<int,int>,int>>q;
+    q.push({{1,1},0});
+    map[1][1]=1;
+    while(!q.empty()){
+        int curY = q.front().first.first;
+        int curX = q.front().first.second;
+        int d = q.front().second;
+        q.pop();
 
-		if (curY == n - 1 && curX == m - 1) {
-			cout << "YES" << "\n";
-			cout << d;
-			exAns = true;
-			break;
-		}
+        if(curY==n && curX == m){
+            answer = d;
+            break;
+        }
+        for(int i=0;i<4;i++){
+            int ny = curY +dy[i];
+            int nx = curX +dx[i];
 
-		for (int i = 0; i < 4; i++) {
-			int ny = curY + dy[i];
-			int nx = curX + dx[i];
-			if (OOB(ny, nx) || map[ny][nx])
-				continue;
-			map[ny][nx] = 1;
-			q.push({ ny,nx,d + 1 });
-		}
+            if(ny>n || ny<=0 || nx>m || nx<=0)
+                continue;
 
-	}
-
+            if(map[ny][nx]==1)
+                continue;
+            map[ny][nx]=1;
+            q.push({{ny,nx},d+1});
+        }
+    }
 }
-
-
-
 int main() {
+    cin>>n>>m;
+    cin>>k;
 
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+    int y=0,x=0,d=0;
 
+    fill(range[0],range[3001],-1);
+    for(int i=0;i<k;i++){
+        cin>>y>>x>>d;
+        range[y][x]  = max(range[y][x],d);
+    }
 
-	cin >> n >> m;
-	cin >> k;
-	vector<vector<int>>tmp(n,vector<int>(m,0));
-	map = tmp;
-	int a = 0, b = 0, c = 0;
-	int MoveR[4] = { -1,1,1,-1 };
-	int MoveC[4] = { 1,1,-1,-1 };
-	for (int i = 0; i < k; i++) {
-		cin >> a >> b >> c;
-		map[a-1][b-1] = 1;
-		int NowR = a-1;
-		int NowC = b - c-1;
-		for (int j = 0; j < 4; j++) {
-			for (int k = 0; k < c; k++) {
-				NowR += MoveR[j];
-				NowC += MoveC[j];
-				if ((NowR >= 0) && (NowR <= n-1) && (NowC >= 0) && (NowC <= m-1)) {
-					map[NowR][NowC] = 1;
-				}
-			}
-		}
-		
-	}
-	/*for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++)
-			cout << map[i][j];
-		cout << endl;
-	}*/
-	
-	bfs();
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(range[i][j]>=0){    
+                mark(i,j,range[i][j]);
+            }
+        }
+    }
 
-	
-	if (!exAns)
-		cout << "NO";
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=m;j++){
+    //         cout<<map[i][j]<<' ';
+    //     }
+    //     cout<<endl;
+    // }
+
+    bfs();
+    
+
+    if(answer==-1)
+        cout<<"NO"<<endl;
+    else{
+        cout<<"YES"<<endl;
+        cout<<answer;
+            
+    }
+    return 0;
 }
