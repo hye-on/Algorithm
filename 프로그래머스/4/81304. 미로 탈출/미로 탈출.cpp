4128d0ab22ddaf34285f92a71ddb5813097e32d1
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-01-27 16:45:18 +0900

    [level 4] Title: 미로 탈출, Time: 3.86 ms, Memory: 8.35 MB -BaekjoonHub

--- 프로그래머스/4/81304. 미로 탈출/미로 탈출.cpp
+++ 프로그래머스/4/81304. 미로 탈출/미로 탈출.cpp
@@ -1,115 +1,52 @@
 #include <string>
 #include <vector>
-#include<algorithm>
-#include<queue>
-#include<tuple>
-#include<iostream>
+#include <set>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-//최단거리 다익스트라
-//dist[i][state] i번째 방문 state 00101011일때 i에 도달하는 최단 경로
-//
-int d[1004][1024];
-vector<pair<int,int>>adj[1004]; 
-vector<pair<int,int>>adjrev[1004]; 
-int trapidx[1004];
-int answer = -1;
-
-bool bitmask(int state,int cur){
-    return (1<<trapidx[cur]) & state;
-}
-   
-void dijkstra(int start,int end){
-    
-    priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
-    pq.push({d[start][0],start,0});
-    
-    while(!pq.empty()){
-        int dist ,cur,state;
-        tie(dist,cur,state) = pq.top();
-        pq.pop();
-         
-        if(cur==end){
-            answer  = dist;
-            
-            return;
-        }
-        
-        if(d[cur][state] <dist)
-            continue;
-        
-        //정방향 간선 확인
-        for(auto p : adj[cur]){
-            int nxt = p.first;
-            int cost = p.second;
-            int rev=0;
-            //현재 트랩을 밟은 상태이면 뒤집음
-            if(trapidx[cur] !=-1 && bitmask(state,cur))
-                rev^=1;
-            //다음 노드도 트랩이면 다시 상태를 뒤집음
-            if(trapidx[nxt] != -1 && bitmask(state,nxt))
-                rev^=1;
-            //현재 있는 노드 or 다음 노드 중 1개만 밟았으면 정방향 간선이 뒤집어져 있음
-            if(rev)
-                continue;
-            //두번이나 0번 밟았으면 뒤집어져 있지 않음
-            int next_state = state;
-            //다음 노드가 트랩이면 상태에 추가
-            if(trapidx[nxt] != -1 )
-                next_state ^= (1<<trapidx[nxt]);
-            
-            if(d[nxt][next_state] > cost + dist){
-                d[nxt][next_state] = cost +dist;
-                pq.push({d[nxt][next_state],nxt,next_state});
-            }
-            
-        }
-        
-        //역방향 간선 확인
-        for(auto p : adjrev[cur]){
-            int nxt = p.first; 
-            int cost = p.second;
-            int rev=0;
-            if(trapidx[cur]!=-1 && bitmask(state,cur)) rev^=1;
-            if(trapidx[nxt]!=-1 && bitmask(state,nxt)) rev^=1;
-            
-            if(!rev)
-                continue;
-            int next_state = state;
-            
-            if(trapidx[nxt] !=-1)
-                next_state ^= (1<<trapidx[nxt]);
-            
-            if(d[nxt][next_state] >cost + dist){
-                d[nxt][next_state] = cost + dist;
-                pq.push({d[nxt][next_state],nxt,next_state});
-            } 
-        }
-    }
+#define MAX 1000
+//6:43
+//dfs
+// dfs를 다 해서 -> 경로 찾음 -> 이게 가능한 경로인지 확인
+// 방향이 안맞으면 한번 더 들리면 갈 수 있음 -> 비용 추가
+vector<vector<pair<int,bool>>> link(MAX+1, vector<pair<int,bool>>(MAX+1));
+int des;
+bool visit[MAX+1];
+set<int>s;
+void dfs(int node,int cost){
     
 }
 int solution(int n, int start, int end, vector<vector<int>> roads, vector<int> traps) {
-    
-    for(auto road : roads){
-        int u = road[0];
-        int v = road[1];
-        int val = road[2];
-        adj[u].push_back({v,val});
-        adjrev[v].push_back({u,val});  //역방향     
+    int answer = 0;
+   
+    des= end;
+    for(auto t : traps)
+        s.insert(t);
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<n+1;j++ ){
+            link[i][j] ={3001,0};
+        }
+         
+    }
+    for(int i=0;i<roads.size();i++){
+        int a = roads[i][0];
+        int b = roads[i][1];
+        int c = roads[i][2];
+        cout<<link[a][b].first<<endl;
+        if(link[a][b].first>c){
+            link[a][b] = {c,1}; //활성화 된 길
+            link[b][a] = {c,0};          
+        }        
     }
     
-    fill(trapidx,trapidx+n+1,-1);
-    
-    for(int i=0;i<traps.size();i++){
-        trapidx[traps[i]] =i;
+     for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++ ){
+            cout<<link[i][j].first<<" ";
+        }
+         cout<<endl;
     }
-    
-    for(int i=1;i<=n;i++)
-        fill(d[i],d[i]+1024,30000000);
-    
-    d[start][0] =0;
-    dijkstra(start,end);
     
     return answer;
 }
