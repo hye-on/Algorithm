4c5dafe06585354abf1a05f30723f7e8f93b7ec9
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-02-01 22:17:04 +0900

    [Gold II] Title: Cubeditor, Time: 76 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1701. Cubeditor/Cubeditor.cc
+++ 백준/Gold/1701. Cubeditor/Cubeditor.cc
@@ -1,36 +1,28 @@
 #include <iostream>
-#include<map>
-#include<algorithm>
 
 using namespace std;
+//5000*5000*5000-> 75000000000
+//투포인터로 하고 최적화
+string s;
 
-map<string,int>m;
-
-//4:47 ~5:11
-//투포인터
-string s="";
-int cnt;
-int ans;
 int main() {
-    
     cin>>s;
-    for(int i=0;i<s.size()-ans;i++){          
-        for(int j=i+1;j<s.size();j++){    
-            cnt=0;
-           if(s[i]==s[j]){
-               int gap = j-i;
-               for(int k=j;k<s.size();k++){
-                   if(s[k-gap]==s[k]){
-                       cnt++;
-                       ans = max(cnt,ans);
-                   }else{
-                       
-                       break;
-                   }
-               }
-           }
+    int ans=0;
+    for(int i=0;i<s.size();i++){
+        for(int j=i+1;j<s.size()-ans;j++){
+            if(s[i]==s[j]){
+                int cnt=0;
+                for(int k=0;j+k<s.size();k++){
+                    if(s[i+k]==s[j+k]){
+                        cnt++;
+                    }else{
+                        break;
+                    }
+                }
+                ans = max(ans,cnt);
+            }
         }
     }
-
     cout<<ans;
+    return 0;
 }
