3e01032102a8c37b289aa6dd906ba5dd661a0b04
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-02-11 19:49:16 +0900

    Time: 4 ms (92.23%), Space: 20.7 MB (84.14%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,60 +1,42 @@
 class Solution {
+    
+    int dfs(int r,int c ,vector<vector<int>>& matrix,vector<vector<int>>& dp,int n, int m,int prev){
+
+        if(r<0 || r>=n || c<0 || c>=m || prev>=matrix[r][c]) return 0;
+
+        if(dp[r][c]!=-1) 
+            return dp[r][c];
+
+        int ans=1;
+        int dx[] = {0,1,0,-1};
+        int dy[] = {1,0,-1,0};
+
+        for(int i=0;i<4;i++){
+            int nr = r+dy[i];
+            int nc = c+dx[i];
+            ans = max(ans,dfs(nr,nc,matrix,dp,n,m,matrix[r][c])+1);
+        }
+        dp[r][c] = ans;
+        return  dp[r][c];
+    }
+
 public:
 //백준에 비슷한 문제 있음 bfs+ 메모리 제이션 
-    int answer=0;
+    
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-      //  matrix = {{1,4,7,9},{0,3,8,5},{3,6,0,6},{1,4,5,6}};
+     
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m));
-        fill(dp.begin(),dp.end(),vector<int>(m,0));
+        int answer=0;
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+       
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(dp[i][j]==0)
-                    bfs(i,j,dp,n,m,matrix);
-               
+                answer = max(answer,dfs(i,j,matrix, dp,n,m,-1));             
             }
         }
-         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-               answer = max(answer,dp[i][j]);
-             //   cout<<dp[i][j]<<' ';
-            }
-          //  cout<<endl;
-        }
+        
         return answer;
     }
 
-
-
-
-    void bfs(int sy,int sx,vector<vector<int>>&dp, int& n,int& m,vector<vector<int>>& matrix){
-        int dy[] ={0,1,0,-1};
-        int dx[] = {1,0,-1,0};
-        queue<pair<pair<int,int>,int>>q;
-
-        q.push({{sy,sx},1});
-        dp[sy][sx]=1;
-
-        while(!q.empty()){
-            int cury = q.front().first.first;
-            int curx = q.front().first.second;
-            int d = q.front().second;
-            q.pop();
-           
-            for(int i=0;i<4;i++){
-                int ny = cury + dy[i];
-                int nx = curx + dx[i];
-
-                if(ny>=n || ny<0 || nx>=m || nx<0)
-                    continue;
-                if(matrix[ny][nx]<=matrix[cury][curx])
-                    continue;
-               if(dp[ny][nx] < d+1){
-                dp[ny][nx] = d+1;
-                q.push({{ny,nx},d+1});
-                }
-            }
-        }
-    }
 };
