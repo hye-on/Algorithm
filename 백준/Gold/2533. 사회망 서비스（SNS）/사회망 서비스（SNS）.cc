b17211667a51c73416b70adfb3dc146c9e179c68
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-02-26 12:57:40 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1404 ms, Memory: 109288 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -9,34 +9,35 @@ using namespace std;
 int n;
 vector<vector<int>>edges;
 vector<vector<int>>dp;
+bool visited[1000001];
 
-int findEarlyAdaptor(int cur,int flag,int parent){
+void findEarlyAdaptor(int cur){
 
     
     //내가 얼리어답터면 연결된 애들 다 얼러 어답터 거나 아니거나
     //내가 얼리어답터가 아니면 연결된 애들 다 얼리 어답터
-    if(dp[cur][flag]!=MAX)
-        return dp[cur][flag];
-    
-    int ret=flag;
+
+    visited[cur]= true;
+    // 1이 얼리 어답터
+    dp[cur][1]  = 1;
     //내가 얼리 어답터
     for(auto nxt : edges[cur]){
-        if(nxt==parent)
-            continue;
-        if(flag)
-            ret += min(findEarlyAdaptor(nxt,1,cur),findEarlyAdaptor(nxt,0,cur));
-        else
-            ret += findEarlyAdaptor(nxt,1,cur);
+       if(visited[nxt]) 
+           continue;
+        
+        findEarlyAdaptor(nxt);
+        dp[cur][0] += dp[nxt][1];
+        dp[cur][1] +=  min(dp[nxt][1],dp[nxt][0]);
+        
             
     }
-     
-    return dp[cur][flag] = ret;
+   
 }
 
 int main() {
     cin>>n;
     edges.resize(n+1);
-    dp.resize(n+1,vector<int>(2,MAX));
+    dp.resize(n+1,vector<int>(2));
         
     int inputA=0,inputB=0;
     
@@ -46,9 +47,8 @@ int main() {
         edges[inputB].push_back(inputA);
     }
 
-    findEarlyAdaptor(1,1,0);
-    findEarlyAdaptor(1,0,0);
-    
+    findEarlyAdaptor(1);
+
     cout<<min(dp[1][0],dp[1][1])<<endl;
 
     
