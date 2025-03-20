7cf987fa32751f430d5982c9e701d5e7dfc54e93
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-03-20 14:06:54 +0900

    [Gold V] Title: 캡틴 이다솜, Time: 20 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Gold/1660. 캡틴 이다솜/캡틴 이다솜.cc
+++ 백준/Gold/1660. 캡틴 이다솜/캡틴 이다솜.cc
@@ -1,63 +1,34 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-//#include<cstdlib>
+#include <bits/stdc++.h>
 using namespace std;
-long long dp[300002];
+typedef long long ll;
+typedef pair<int, int> pii;
+const int MAX = INT_MAX;
 
+int N;
+int arr[121], dp[300001];
 
 int main()
 {
-	int N;
-	cin >> N;
-	
-	fill_n(dp, 300002, 1e9);
-	
+    ios_base::sync_with_stdio(0); 
+	cin.tie(0);
 
+    for(int i=1; i<121; i++) 
+    {
+        arr[i] = i*(i+1)*(i+2) / 6; 
+    }
 
-	vector<int>tetrahedron;
-	tetrahedron.push_back(0);
-	tetrahedron.push_back(1);
-	
-	//int temp;
-	int a=1, b;
-	
-	for (int i = 2;; i++)
-	{
-		b = a + i;
-		if (tetrahedron[i - 1] + b > 300000) break;
-		tetrahedron.push_back(tetrahedron[i - 1] + b);
-		a = b;
-	}
-	//cout << tetrahedron.size() <<"  "<<tetrahedron[tetrahedron.size()-2]<< endl;
-	/*for (int i = 2; i <= 10; i++)
-	{
-		cout << dp[i] << endl;
-	}*/
-		for (int i = 0; i < tetrahedron.size(); i++)
-		{
-			dp[tetrahedron[i]] = 1;
-		}
+    cin >> N;
+    for(int i=1; i<300001; i++) dp[i]=i; 
 
-		for (int i = 1; i < tetrahedron.size(); i++)
-		{
-			for (int j = tetrahedron[i]; j <= N; j++)
-			{
-				
-				dp[j] = min(dp[j], dp[j - tetrahedron[i]] + 1);
-				//cout << dp[j]<<endl;
+    for(int i=1; i<121; i++)
+    {
+        for(int j=arr[i]; j<=N; j++)
+        {
+            dp[j] = min(dp[j], dp[j-arr[i]] + 1); 
+        }
+    }
 
-			}
-		}
-		cout << dp[N];
+    cout << dp[N];
 
-
-		/*for (int i = 1; i < 8; i++)
-		{
-			cout << tetrahedron[i] << endl;
-		}*/
-
-
-	}
-
-	
+    return 0;
+}
