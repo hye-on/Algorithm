cd25ba574f44ea3f8e393ff45af55a1015abfe2a
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-09-17 22:56:23 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 51.31 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
@@ -1,50 +1,70 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <algorithm>
+
 using namespace std;
 
-//오후 04:13
+//10:24 ~10:47
+//나가는것만 있고 들어오는게 없으면 정점
+// 도넛: 나가는거 하나 들어오는거 하나
+// 막대: 나가는것만 있는 노드, 들어오는것만 있는 노드 있음. or 아무것도 없거나
+// 8자: 들어오는거 2개 나가는거 2개
+vector<pair<int,int>>node_cnt(1000001); //나가는거, 들어오는거
+bool visit[1000001];
 
 
-vector<pair<int,int>>v(1000001); //나간거, 들어온거
-vector<int>num[1000001]; //연결 노드
-bool check[1000001]; //존재 노드
 
 
 vector<int> solution(vector<vector<int>> edges) {
     vector<int> answer;
     answer.resize(4);
-    
-    for(int i=0;i<4;i++)
-        answer[i]=0;
-    
+    int n=0;
+    //초기화. 들어오는간선 나가는 간선 체크
     for(int i=0;i<edges.size();i++){
-        num[edges[i][0]].push_back(edges[i][1]);
-        v[edges[i][0]].first++;
-        v[edges[i][1]].second++;
-        check[edges[i][0]]=true;
-        check[edges[i][1]]=true;
+        int from = edges[i][0];
+        int to = edges[i][1];
+        
+        node_cnt[from].first++;
+        node_cnt[to].second++;
+        n = max(n,from);
+        n = max(n,to);
+        visit[from] =true;
+        visit[to] =true;
     }
-    int cnt=0;
     
-    for(int i=1;i<1000001;i++){
-        if(v[i].first>1 && v[i].second==0){
-            answer[0]=i;      
-            cnt = v[i].first;       
-            for(int j=0;j<num[i].size();j++){
-                v[num[i][j]].second--;
-                
-            }
+    
+    
+    //정점 찾기
+    for(int i=1;i<=n;i++){
+        if(node_cnt[i].first>=2 && node_cnt[i].second==0){
+            answer[0]=i;
             break;
         }
     }
-    
-    for(int i=1;i<1000001;i++){
-        if(v[i].first==2 && v[i].second==2)
-            answer[3]++;
-        if(check[i] && v[i].first == 0 && v[i].second<=1 )
-           answer[2]++;
+    //정점과 관련된 간선 없애기
+    int total =0;
+    for(int i=0;i<edges.size();i++){
+        if(edges[i][0]==answer[0]){
+            node_cnt[edges[i][1]].second--;
+            total++;
+        }
     }
-    answer[1]=cnt-(answer[3]+answer[2]);
+    for(int i=1;i<=n;i++){
+        if(!visit[i])
+            continue;
+        if(i==answer[0])
+            continue;
+        //스틱2
+        if(node_cnt[i].first>=0 && node_cnt[i].second==0){
+            answer[2]++;
+            continue;
+        }
+            
+         //8자3 
+        if(node_cnt[i].first==2 && node_cnt[i].second==2)
+            answer[3]++;
+    }
+    answer[1] = total - answer[2] - answer[3];
+    
     return answer;
 }
