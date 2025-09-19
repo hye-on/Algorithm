954f28098f64c98c1d14fa697b60f8120a9abbdd
조혜온 <68319395+hye-on@users.noreply.github.com>
2025-09-19 22:52:49 +0900

    [level 2] Title: 피로도, Time: 1.23 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,30 +1,39 @@
 #include <string>
 #include <vector>
+#include<iostream>
 
 using namespace std;
-vector<vector<int>> dungeon;
-vector<bool>chk;
+
+//10:32
+//던전 최대 8개
 int answer;
-void dfs(int score,int cnt){
-       
+vector<vector<int>> dungeons;
+vector<bool>visit;
+
+void dfs(int score, int dun_cnt){
     if(score<0)
         return;
-    if(cnt>answer)
-            answer=cnt;
-       
-    for(int i=0;i<dungeon.size();i++){
-        if(chk[i] || score<dungeon[i][0])
-            continue;
-        chk[i]=true;
-        dfs(score-dungeon[i][1],cnt+1);
-        chk[i]=false;
+    if(dun_cnt>answer){
+        answer = dun_cnt;
+        cout<<endl;
     }
     
+    for(int i=0;i<dungeons.size();i++){
+        if(visit[i])
+            continue;
+        //최소 필요 피로도가 낮음
+        if(score<dungeons[i][0])
+            continue;
+        visit[i]=true;
+        dfs(score-dungeons[i][1],dun_cnt+1);
+        visit[i]=false;
+    }
 }
-int solution(int k, vector<vector<int>> dungeons) {
-    
-    dungeon = dungeons;
-    chk.resize(dungeons.size());
+
+int solution(int k, vector<vector<int>> _dungeons) {
+   
+    dungeons = _dungeons;
+    visit.resize(dungeons.size());
     dfs(k,0);
     return answer;
 }
